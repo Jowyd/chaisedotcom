7c1392e57fc81eaaf57b73294946825068630ab6
Lyam <lyamkeovilay74@gmail.com>
2024-10-24 16:15:35 +0200

    iui

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,37 +1,32 @@
 import express, { Application } from "express";
-// import morgan from "morgan";
-// import swaggerUi from "swagger-ui-express";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 
 import { RegisterRoutes } from "./routes/index"; // tsoa va générer ce fichier
 import { errorHandler } from "./middlewares/errorHandler";
-import { AppDataSource } from "./config/datasource";
 
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// app.use(
-//   "/docs",
-//   swaggerUi.serve,
-//   swaggerUi.setup(undefined, {
-//     swaggerOptions: {
-//       url: "/swagger.json",
-//     },
-//   })
-// );
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-    app.listen(PORT, () => {
-      console.log("Server is running on port", PORT);
-    });
+app.use(morgan("tiny"));
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
   })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
+);
 
 RegisterRoutes(app);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
 
 app.use(errorHandler);
---
+++ api/src/config/database.ts
@@ -0,0 +1,6 @@
+import { Sequelize } from "sequelize";
+
+const url = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const sequelize = new Sequelize(url);
+
+export default sequelize;
