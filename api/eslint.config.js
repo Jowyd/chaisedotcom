9c262f025334675dd392689d7b6a34a6c831f311
Lyam <lyamkeovilay74@gmail.com>
2025-01-15 16:15:14 +0100

    Refactor ESLint configuration to streamline TypeScript support and improve rule management

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,53 +1,47 @@
-import eslint from "@eslint/js";
-import * as tseslint from "@typescript-eslint/eslint-plugin";
-import * as tsParser from "@typescript-eslint/parser";
-
-export default [
-  eslint.configs.recommended,
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: "./tsconfig.json",
-        ecmaVersion: 2024,
-        sourceType: "module",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-    rules: {
-      // Code style & format
-      semi: ["error", "always"],
-      quotes: ["error", "single"],
-      indent: ["error", 2],
-      "comma-dangle": ["error", "always-multiline"],
-
-      // TypeScript specifics
-      "@typescript-eslint/explicit-function-return-type": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
-      "@typescript-eslint/no-explicit-any": "error",
-
-      // Best practices
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      "no-debugger": "error",
-      "prefer-const": "error",
-      "no-var": "error",
-    },
-    ignorePatterns: [
-      "node_modules/",
-      "dist/",
-      "**/*.js",
-      "**/*.cjs",
-      "**/*.mjs",
-      "coverage/",
-    ],
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-];
+  rules: {
+    // Code style & format
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    indent: ["error", 2],
+    "comma-dangle": ["error", "always-multiline"],
+
+    // TypeScript specifics
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "error",
+
+    // Best practices
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-debugger": "error",
+    "prefer-const": "error",
+    "no-var": "error",
+  },
+  ignorePatterns: [
+    "node_modules/",
+    "build/",
+    "**/*.js",
+    "**/*.cjs",
+    "**/*.mjs",
+    "coverage/",
+  ],
+  env: {
+    node: true,
+    es2021: true,
+  },
+};
